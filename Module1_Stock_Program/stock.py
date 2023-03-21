a1c2c56ef19ea1b09d924a285870d977b12fb7d7
TheBeanBro <128415118+TheBeanBro@users.noreply.github.com>
2023-03-21 20:49:21 +0000

    Add files via upload

--- Module1_Stock_Program/stock.py
+++ Module1_Stock_Program/stock.py
@@ -1,11 +1,12 @@
 class Stock:
-    def __init__(self):
-        pass
+    def __init__(self, branch={}):
+        self.branch = branch
 
     def add_stock(self):
         choice = input("Choose branch b1/b2: ")
         choice.lower()
         branch1 = {}
+        branch2 = {}
         if choice == 'b1':
             print("branch1: ")
             for i in range(1):
@@ -14,8 +15,17 @@ class Stock:
                 quantity = int(input("Allocate Quantity: "))
 
                 branch1[name] = {"Price: ": [price], "Quantity": [quantity]}
-            print(branch1)
+                self.branch.update(branch1)
+            print(self.branch)
         elif choice == 'b2':
             print("branch2: ")
+            for i in range(1):
+                name = str(input("Name of item: "))
+                price = float(input("Allocate Price: "))
+                quantity = int(input("Allocate Quantity: "))
+
+                branch2[name] = {"Price: ": [price], "Quantity": [quantity]}
+            print(branch2)
+
         else:
             print("ERR: not a valid branch")
