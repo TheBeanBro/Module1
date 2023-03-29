e880f41ea16f4d46d64389005f89af994c2b758f
TheBeanBro <128415118+TheBeanBro@users.noreply.github.com>
2023-03-29 08:23:40 +0200

    Add files via upload

--- Module1_Stock_Program/main.py
+++ Module1_Stock_Program/main.py
@@ -1,30 +1,40 @@
 from stock import Stock
 
 
-def add():
+def add_stock():
     stock = Stock()
     stock.add_stock()
 
 
+def allocate_stock():
+    allocate = Stock()
+    allocate.allocate_stock()
+
+
+def review_stock():
+    review = Stock()
+    review.review_stock()
+
+
 def program():
     print("0: add stock")
-    print("1: remove stock")
+    print("1: allocate stock")
     print("2: review stock")
     print("3: exit program")
     choice = int(input("Answer: "))
     if choice == 0:
-        add()
+        add_stock()
         program()
     elif choice == 1:
-        pass
+        allocate_stock()
+        program()
     elif choice == 2:
-        review_list = Stock()
-        review_list.review()
+        review_stock()
         program()
     elif choice == 3:
         exit()
     else:
-        print("ERR: please give the corresponding number which is relavent to your choice")
+        print("ERR: please give a number which is relavent to your choice")
 
 
 program()
--- Module1_Stock_Program/stock.py
+++ Module1_Stock_Program/stock.py
@@ -1,48 +1,27 @@
 class Stock:
-    def __init__(self, branch1={}, branch2={}, itm=""):
-        self.branch1 = branch1
-        self.branch2 = branch2
-        self.itm = itm
+    def __init__(self, stock=[], my_items={}) -> None:
+        self.stock = stock
+        self.my_items = my_items
 
     def add_stock(self):
-        choice = input("Choose branch b1/b2: ")
-        choice.lower()
+        stock = input('Stock: ')
+        stock.lower()
 
-        # If choice ==>
-        if choice == 'b1':
+        self.stock.append(stock)
+        for element in self.stock:
+            print(element)
 
-            # Branch 1
-            print("branch1: ")
-            for i in range(1):
-                # Ask for name
-                itm = str(input("Name of item: "))
+    def allocate_stock(self):
+        for item in self.stock:
+            self.my_items = {item: []}
+            print(self.my_items)
 
-                # Add name to list and print it
-                self.branch1[itm] = {}
-            print(self.branch1)
+            price = float(input('Price: '))
+            self.my_items[item].append(price)
 
-        # If choice == 'b2'
-        elif choice == 'b2':
+    def remove_stock(self):
+        for item in self.stock:
+            print(self.my_items)
 
-            # Branch 2
-            print("branch2: ")
-            for i in range(1):
-                # Ask for name
-                itm = str(input("Name of item: "))
-
-                # Add name to list and print it
-                self.branch2[itm] = {}
-            print(self.branch2)
-
-        else:
-            print("ERR: not a valid branch")
-
-    def review(self):
-        choice = input("Choose branch b1/b2: ")
-        choice.lower()
-        if choice == 'b1':
-            print(self.branch1)
-        elif choice == 'b2':
-            print(self.branch2)
-        else:
-            print("ERR: please choose b1/b2")
+    def review_stock(self):
+        print()
