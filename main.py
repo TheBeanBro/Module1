3688e1041441b72c027e1d67214e836ebd32d987
TheBeanBro <128415118+TheBeanBro@users.noreply.github.com>
2023-04-24 17:28:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ class Stock:
         if branch_name not in self.branches:
             # Tell the user that the branch they entered doesn't exist in branches
             print(f"The branch: {branch_name} does not exist.")
-            # Tell computer result of function is none
+            # Tell computer result of function is None
             return
         # The branches dictionary is equivilant to {Name of branch: {Item: price, quantity}}
         self.branches[branch_name][item_name] = price, qty
 
-    # A function that removes items from the desired branch
-    def remove_item(self, branch_name, item_name):
+    # A function that destroys items from the desired branch
+    def destroy_item(self, branch_name, item_name):
         # Make sure the branch name exists within the branches dictionary
         if branch_name not in self.branches:
             # Tell the user the branch does not exist
@@ -46,8 +46,10 @@ class Stock:
             for item_name, price in branch_items.items():
                 print(f"\t{item_name}: {price}")
 
+# Insantiate an object of stock
 stock = Stock()
 
+# Infinite loop
 while True:
     print("\n1. Add branch\n2. Add item\n3. Remove item\n4. Print stock\n5. Exit")
     choice = input("Enter your choice (1-5): ")
@@ -68,13 +70,15 @@ while True:
         stock.add_items(branch_name, item_name, price, qty)
         if branch_name not in stock.branches:
             print(f"{item_name} added to branch: {branch_name} | unsuccessful. (branch does not exist).")
+        elif item_name in stock.branches[branch_name]:
+            print(f"There is no item {item_name} in the branch: {branch_name}")
         else:
             print(f"{item_name} added to branch: {branch_name} | successfull.")
 
     elif choice == "3":
         branch_name = input("Enter the name of the branch which you want to remove an item from: ")
         item_name = input("Enter item name: ")
-        stock.remove_item(branch_name, item_name)
+        stock.destroy_item(branch_name, item_name)
         if branch_name not in stock.branches:
             print(f"{item_name} removed from the branch: {branch_name} | unsuccessful (branch does not exist).")
         else:
